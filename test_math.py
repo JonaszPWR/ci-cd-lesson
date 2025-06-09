0870ff7211c8aa35b4a368099e5eba14f9a073cb
unknown <272577@student.pwr.edu.pl>
2025-06-09 10:35:11 +0200

    pytest syntax

--- test_math.py
+++ test_math.py
@@ -1,13 +1,13 @@
 import pytest
 import math_functions
 
-@pytest.mark.parameterize("a, b, expected", [(1,2,3), (2,3,5), (3,4,7), (4,5,9)])
+@pytest.mark.parametrize ("a, b, expected", [(1,2,3), (2,3,5), (3,4,7), (4,5,9)])
 
 def test_add(a, b, expected):
     result = math_functions.add(a,b)
     assert result == expected
 
-@pytest.mark.parameterize("a, b, expected",[ (1 , 2 , - 1 ), (2 , 3, - 1 ),
+@pytest.mark.parametrize ("a, b, expected",[ (1 , 2 , - 1 ), (2 , 3, - 1 ),
                                              (3 , 4 , - 1 ), (4 , 5 , - 1 ) ])
 def test_subtract (a , b , expected ):
     result = math_functions.subtract (a , b)
