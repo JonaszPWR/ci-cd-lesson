1d4e8afdbbf2b815826dfafb72e1c9da4a654601
unknown <272577@student.pwr.edu.pl>
2025-06-09 10:39:39 +0200

    run black

--- math_functions.py
+++ math_functions.py
@@ -1,13 +1,21 @@
-'''Own math functions'''
-def add ( a : int , b : int ) -> int:
-    '''Adder'''
+"""Own math functions"""
+
+
+def add(a: int, b: int) -> int:
+    """Adder"""
     return a + b
-def subtract ( a : int , b : int ) -> int:
-    '''Adder but the second number is multiplied by -1'''
+
+
+def subtract(a: int, b: int) -> int:
+    """Adder but the second number is multiplied by -1"""
     return a - b
-def multiply ( a : int , b : int ) -> int:
-    '''Product generator'''
+
+
+def multiply(a: int, b: int) -> int:
+    """Product generator"""
     return a * b
-def divide ( a : int , b : int ) -> float:
-    '''Product generator but the second number is raised to -1'''
+
+
+def divide(a: int, b: int) -> float:
+    """Product generator but the second number is raised to -1"""
     return a / b
--- test_math.py
+++ test_math.py
@@ -1,27 +1,30 @@
 import pytest
 import math_functions
 
-@pytest.mark.parametrize ("a, b, expected", [(1,2,3), (2,3,5), (3,4,7), (4,5,9)])
 
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7), (4, 5, 9)])
 def test_add(a, b, expected):
-    result = math_functions.add(a,b)
+    result = math_functions.add(a, b)
     assert result == expected
 
-@pytest.mark.parametrize ("a, b, expected",[ (1 , 2 , - 1 ), (2 , 3, - 1 ),
-                                             (3 , 4 , - 1 ), (4 , 5 , - 1 ) ])
-def test_subtract (a , b , expected ):
-    result = math_functions.subtract (a , b)
+
+@pytest.mark.parametrize(
+    "a, b, expected", [(1, 2, -1), (2, 3, -1), (3, 4, -1), (4, 5, -1)]
+)
+def test_subtract(a, b, expected):
+    result = math_functions.subtract(a, b)
     assert result == expected
 
-@pytest . mark . parametrize ( "a , b , expected " , [ (1 , 2 , 2 ), (2 , 3 ,6 ),
-                                                       (3 , 4 , 12 ), (4 , 5 , 20 ) ])
 
-def test_multiply (a , b , expected ):
-    result = math_functions.multiply (a , b)
+@pytest.mark.parametrize(
+    "a , b , expected ", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)]
+)
+def test_multiply(a, b, expected):
+    result = math_functions.multiply(a, b)
     assert result == expected
-@pytest . mark . parametrize ( "a , b , expected " , [ (1 , 2 , 0.5 ), (3 ,4 , 0.75 ),
-                                                       (4 , 5 , 0.8 ) ])
 
-def test_divide (a , b , expected ) :
-    result = math_functions.divide (a , b)
+
+@pytest.mark.parametrize("a , b , expected ", [(1, 2, 0.5), (3, 4, 0.75), (4, 5, 0.8)])
+def test_divide(a, b, expected):
+    result = math_functions.divide(a, b)
     assert result == expected
