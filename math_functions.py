a6c7fd53098042b28a6a3e6cf191cfa0f0909548
unknown <272577@student.pwr.edu.pl>
2025-06-09 10:13:13 +0200

    linting fix

---
+++ math_functions.py
@@ -0,0 +1,13 @@
+'''Own math functions'''
+def add ( a : int , b : int ) -> int:
+    '''Adder'''
+    return a + b
+def subtract ( a : int , b : int ) -> int:
+    '''Adder but the second number is multiplied by -1'''
+    return a - b
+def multiply ( a : int , b : int ) -> int:
+    '''Product generator'''
+    return a * b
+def divide ( a : int , b : int ) -> float:
+    '''Product generator but the second number is raised to -1'''
+    return a / b
